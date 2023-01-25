a8b3c0f35b163a6c92b11d465f80e0874fc3f41d
GHHag <gustav.hagenblad@gmail.com>
2023-01-25 13:17:19 +0100

    bash file

--- build.sh
+++ build.sh
@@ -6,18 +6,29 @@ echo "postgresql - $pg_user password:"
 read pg_password
 echo "postgresql port:"
 read pg_port
-
+echo "superadmin username:"
+read superadmin_username
+echo "superadmin password:"
+read superadmin_password
 echo ""
-echo $pg_user
-echo $pg_password
-echo $pg_port
+echo "Confirm credentials"
+echo "postgresql user: " $pg_user
+echo "postgresql " $pg_user " password: " $pg_password
+echo "postgresql port: " $pg_port
+echo "superadmin username: " $superadmin_username
+echo "superadmin password: " $superadmin_password
 echo "Press enter to proceed"
+read proceed
 
-PGPASSWORD=$pg_password psql -U $pg_user
-\i backend/db/chat_app.sql
+PGPASSWORD=$pg_password psql -U $pg_user -f backend/db/chat_app.sql
 
 cd backend
-node server/server.js &
+npm install
+node server/server.js
 
 cd ../frontend
+npm install
 npm run dev
+
+curl -X POST -H "Content-Type: application/json" -d '{"username": "'$superadmin_username'", "password": "'$superadmin_password'"}' http://localhost:8000/api/user/register
+PGPASSWORD=$pg_password psql -U $pg_user -d chat_app -c "UPDATE users SET user_role = 'superadmin' WHERE username = '"$superadmin_username"'"
